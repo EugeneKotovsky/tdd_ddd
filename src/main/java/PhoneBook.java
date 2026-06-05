b354be3dcec88a43226dc151eebf2e49f79c868a
enpolezhnyy <d3mi3n@mail.ru>
2026-06-05 21:15:27 +0700

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,16 @@
+import java.util.*;
+
 public class PhoneBook {
+    private final Map<String, String> nameToNumber = new HashMap<>();
+    private final Map<String, String> numberToName = new HashMap<>();
+    private final Set<String> sortedNames = new TreeSet<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (!nameToNumber.containsKey(name)) {
+            nameToNumber.put(name, number);
+            numberToName.put(number, name);
+            sortedNames.add(name);
+        }
+        return nameToNumber.size();
     }
 }
